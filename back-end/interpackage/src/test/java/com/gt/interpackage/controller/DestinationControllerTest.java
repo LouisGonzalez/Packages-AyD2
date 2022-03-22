f02c6f558daca5dc3a9bc9de9787890918f6dc40
BryanGmz <brgg13@gmail.com>
2022-03-21 23:37:49 -0600

    Renombrar paquete test controller

--- back-end/interpackage/src/test/java/com/gt/interpackage/controller/DestinationControllerTest.java
+++ back-end/interpackage/src/test/java/com/gt/interpackage/controller/DestinationControllerTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.gt.interpackage.FeeController;
+package com.gt.interpackage.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gt.interpackage.controller.DestinationController;
