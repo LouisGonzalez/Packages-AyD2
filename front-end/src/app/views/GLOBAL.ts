1172812b2b9190e7442a376578eedcdb13b5e6bb
Luis <louiscrack50@gmail.com>
2022-05-07 10:37:32 -0600

    change the backend urls

--- front-end/src/app/views/GLOBAL.ts
+++ front-end/src/app/views/GLOBAL.ts
@@ -4,27 +4,28 @@ export var GLOBAL = {
     urlApi: 'https://interpackage-gt-backend.herokuapp.com/api/v1',
 
 
-    
+
     /**
-     * 
-     *  URL's mircroservicios desplegados localmente 
-     */ 
-    urlMicroserviceAdministration:  'http://localhost:8080/api/administration/v1',
+     *
+     *  URL's mircroservicios desplegados localmente
+     */
+
+/*    urlMicroserviceAdministration:  'http://localhost:8080/api/administration/v1',
     urlMicroserviceAuthentication:  'http://localhost:8081/api/authentication/v1',
     urlMicroserviceOperator:        "http://localhost:8082/api/operator/v1",
     urlMicroserviceRecepcionist:    'http://localhost:8083/api/recepcionist/v1',
     urlMicroserviceReports:         'http://localhost:8084/api/reports/v1',
-    
+  */
     /**
      * URL's microservicios desplegados en heroku
      */
-    /*
+
     urlMicroserviceAdministration:  'https://interpackage-admin.herokuapp.com/api/administration/v1',
     urlMicroserviceAuthentication:  'https://interpackage-auth.herokuapp.com/api/authentication/v1',
     urlMicroserviceOperator:        'https://interpackage-operator.herokuapp.com/api/operator/v1',
     urlMicroserviceRecepcionist:    'https://interpackage-recep.herokuapp.com/api/recepcionist/v1',
     urlMicroserviceReports:         'https://interpackage-reports.herokuapp.com/api/reports/v1',
-    */
+
     querysUrl: 'https://api-dummie.herokuapp.com/',
     ERROR_REQUIRED: 'Este campo es requerido.',
     ERROR_MIN: 'El valor mínimo debe ser 0.',
